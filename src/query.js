80e3cdca3faeaee122241a74af80d041ea40619e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-09-22 15:56:31 +0200

    Overpass does not understand \d in regexp -> replace by [0-9]

--- src/query.js
+++ src/query.js
@@ -50,7 +50,7 @@ function parts (date, options={}) {
       }
     }
     if (date.match(/^\d{4}-\d{2}$/)) {
-      possibilities.push(optPending0(date + '(-\\d{2})?'))
+      possibilities.push(optPending0(date + '(-[0-9]{2})?'))
 
       if (!options.strict) {
         possibilities.push(optPending0(date.substr(0, 4)))
@@ -62,7 +62,7 @@ function parts (date, options={}) {
     }
     if (date.match(/^\d{4}$/)) {
       if (date[3] !== '0') {
-        possibilities.push(optPending0(date + '(-\\d{2}(-\\d{2})?)?'))
+        possibilities.push(optPending0(date + '(-[0-9]{2}(-[0-9]{2})?)?'))
       }
 
       if (!options.strict) {
@@ -73,7 +73,7 @@ function parts (date, options={}) {
       }
     }
     else if (date.match(/^\d{4}s$/)) {
-      possibilities.push(optPending0(date.substr(0, 3)) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+      possibilities.push(optPending0(date.substr(0, 3)) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
 
       if (!options.strict) {
         let cent = parseInt(date.substr(0, 2)) + 1
@@ -84,7 +84,7 @@ function parts (date, options={}) {
       let cent = date.substr(1).padStart('0', 2)
       let year = ('' + ((cent - 1) * 100)).padStart(4, '0')
       possibilities.push('C' + optPending0(('' + cent).padStart('0', 2)))
-      possibilities.push(optPending0(year.substr(0, 2)) + '\\d(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+      possibilities.push(optPending0(year.substr(0, 2)) + '[0-9](0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
     }
   }
   else if (options.op === '<=') {
@@ -96,19 +96,19 @@ function parts (date, options={}) {
       possibilities.push(optPending0(date))
     }
     if (date.match(/^\d{4}-\d{2}$/)) {
-      possibilities.push(optPending0(date) + '(-\\d{2})?')
+      possibilities.push(optPending0(date) + '(-[0-9]{2})?')
     }
     if (date.match(/^\d{4}$/)) {
-      possibilities.push(optPending0(date) + '(-\\d{2}(-\\d{2})?)?')
+      possibilities.push(optPending0(date) + '(-[0-9]{2}(-[0-9]{2})?)?')
     }
     if (date.match(/^\d{3}0s$/)) {
-      possibilities.push(optPending0(date.substr(0, 3)) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+      possibilities.push(optPending0(date.substr(0, 3)) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
     }
     if (date.match(/^C\d+$/)) {
       let cent = date.substr(1).padStart('0', 2)
       let year = ('' + ((date.substr(1) - 1) * 100)).padStart(4, '0')
 
-      possibilities.push(optPending0(year.substr(0, 2)) + '\\d(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+      possibilities.push(optPending0(year.substr(0, 2)) + '[0-9](0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
 
       possibilities.push('C' + optPending0(cent))
     }
@@ -129,7 +129,7 @@ function parts (date, options={}) {
       possibilities = possibilities.concat(parts(date.substr(0, 4), options))
 
       if (date.substr(5, 2) !== '01') {
-        possibilities.push(optPending0(date.substr(0, 5)) + regexpRangeDouble(1, date.substr(5, 2) - 1) + '(-\\d{2})?')
+        possibilities.push(optPending0(date.substr(0, 5)) + regexpRangeDouble(1, date.substr(5, 2) - 1) + '(-[0-9]{2})?')
       }
 
       if (!options.strict) {
@@ -140,7 +140,7 @@ function parts (date, options={}) {
       possibilities = possibilities.concat(parts(date.substr(0, 3) + '0s', options))
 
       if (date[3] !== '0') {
-        possibilities.push(optPending0(date.substr(0, 3)) + regexpRange(0, date[3] - 1) + '(-\\d{2}(-\\d{2})?)?')
+        possibilities.push(optPending0(date.substr(0, 3)) + regexpRange(0, date[3] - 1) + '(-[0-9]{2}(-[0-9]{2})?)?')
       }
 
       if (!options.strict) {
@@ -152,10 +152,10 @@ function parts (date, options={}) {
       possibilities = possibilities.concat(parts('C' + cent , options))
 
       if (date[2] > 0) {
-        possibilities.push(date.substr(0, 2) + regexpRange(0, date[2] - 1) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+        possibilities.push(date.substr(0, 2) + regexpRange(0, date[2] - 1) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
       }
       if (date[0] === '0') {
-        possibilities.push(date.substr(1, 1) + regexpRange(0, date[2] - 1) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+        possibilities.push(date.substr(1, 1) + regexpRange(0, date[2] - 1) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
       }
 
       if (!options.strict) {
@@ -170,17 +170,17 @@ function parts (date, options={}) {
 
       for (let i = 0; i < 2; i++) {
         if (cent[i] > 0) {
-          possibilities.push('C' + optPending0(cent.substr(0, i) + '0') + '\\d'.repeat(1 - i))
+          possibilities.push('C' + optPending0(cent.substr(0, i) + '0') + '[0-9]'.repeat(1 - i))
         }
         if (cent[i] > 1) {
-          possibilities.push('C' + optPending0(cent.substr(0, i)) + regexpRange(1, cent[i] - 1) + '\\d'.repeat(1 - i))
+          possibilities.push('C' + optPending0(cent.substr(0, i)) + regexpRange(1, cent[i] - 1) + '[0-9]'.repeat(1 - i))
         }
 
         if (year[i] > 0) {
-          possibilities.push(optPending0(year.substr(0, i) + '0') + '\\d'.repeat(2 - i) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+          possibilities.push(optPending0(year.substr(0, i) + '0') + '[0-9]'.repeat(2 - i) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
         }
         if (year[i] > 1) {
-          possibilities.push(optPending0(year.substr(0, i)) + regexpRange(1, year.substr(i, 1) - 1) + '\\d'.repeat(2 - i) + '(0s|\\d(-\\d{2}(-\\d{2})?)?)')
+          possibilities.push(optPending0(year.substr(0, i)) + regexpRange(1, year.substr(i, 1) - 1) + '[0-9]'.repeat(2 - i) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
         }
       }
     }
