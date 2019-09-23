f5877215c2e45f76c6216653623fe5dfed5f2fa8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-09-23 18:42:06 +0200

    Support > and >=

--- src/query.js
+++ src/query.js
@@ -1,4 +1,6 @@
 let earlyMidLate = [ 'early', 'mid', 'late' ]
+let maxYear = '2299'
+let maxCent = maxYear.substr(0, 2) + 1
 
 function arrayUnique (arr) {
   return Array.from(new Set(arr))
@@ -136,9 +138,9 @@ function parts (date, options={}) {
       possibilities.push('(|early |mid |late )' + optPending0(year.substr(0, 2)) + '[0-9](0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
     }
   }
-  else if (options.op === '<=') {
+  else if (options.op === '<=' || options.op === '>=') {
     let opt = JSON.parse(JSON.stringify(options))
-    opt.op = '<'
+    opt.op = options.op[0]
     possibilities = possibilities.concat(parts(date, opt))
 
     if (date.match(/^\d{4}-\d{2}-\d{2}$/)) {
@@ -269,6 +271,119 @@ function parts (date, options={}) {
       }
     }
   }
+  else if (options.op === '>') {
+    if (date.match(/^\d{4}-\d{2}-\d{2}$/)) {
+      possibilities = possibilities.concat(parts(date.substr(0, 7), options))
+
+      let p = range(Math.floor((date.substr(8, 2) - 1) / 31 * 3) + 1, 2)
+        .map(i => earlyMidLate[i])
+      if (!options.strict && date.substr(8, 2) !== '31') {
+        p.push(earlyMidLate[Math.floor((date.substr(8, 2)) / 31 * 3)])
+      }
+      if (p.length) {
+        possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 7))
+      }
+
+      if (date.substr(8, 2) !== '31') {
+        possibilities.push(optPending0(date.substr(0, 8) + regexpRangeDouble(parseInt(date.substr(8, 2)) + 1, 31)))
+      }
+
+      if (!options.strict) {
+        if (date.substr(8, 2) !== '31') {
+          possibilities.push(optPending0(date.substr(0, 7)))
+        }
+        if (date.substr(5, 5) !== '12-31') {
+          possibilities.push('(|late )' + optPending0(date.substr(0, 4)))
+        }
+        if (date.substr(3, 7) !== '9-12-31') {
+          possibilities.push('(|late )' + optPending0(date.substr(0, 3)) + '0s')
+        }
+      }
+    }
+    if (date.match(/^\d{4}-\d{2}$/)) {
+      possibilities = possibilities.concat(parts(date.substr(0, 4), options))
+
+      let p = range(Math.floor((parseInt(date.substr(5, 2)) - 1) / 12 * 3) + 1, 2)
+        .map(i => earlyMidLate[i])
+      if (!options.strict && date.substr(5, 2) !== '12') {
+        p.push(earlyMidLate[Math.floor((parseInt(date.substr(5, 2))) / 12 * 3)])
+      }
+      if (p.length) {
+        possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 4))
+      }
+
+      if (date.substr(5, 2) !== '12') {
+        possibilities.push(optPending0(date.substr(0, 5)) + regexpRangeDouble(parseInt(date.substr(5, 2)) + 1, 12) + '(-[0-9]{2})?')
+      }
+
+      if (!options.strict) {
+        if (date.substr(5, 2) !== '12') {
+          possibilities.push(optPending0(date.substr(0, 4)))
+        }
+        if (date.substr(3, 4) !== '9-12') {
+          possibilities.push(optPending0(date.substr(0, 3)) + '0s')
+        }
+      }
+    }
+    if (date.match(/^\d{4}$/)) {
+      possibilities = possibilities.concat(parts(date.substr(0, 3) + '0s', options))
+
+      let p = range(Math.floor((date.substr(3, 1)) / 10 * 3) + 1, 2)
+        .map(i => earlyMidLate[i])
+      if (!options.strict && date.substr(3, 1) !== '9') {
+        p.push(earlyMidLate[Math.floor((date.substr(3, 1)) / 10 * 3)])
+      }
+      if (p.length) {
+        possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 3) + '0s')
+      }
+
+      if (date[3] !== '9') {
+        possibilities.push('(|early |mid |late )' + optPending0(date.substr(0, 3)) + regexpRange(parseInt(date[3]) + 1, 9) + '(-[0-9]{2}(-[0-9]{2})?)?')
+      }
+
+      if (!options.strict && date.substr(3, 1) !== '9') {
+        possibilities.push(optPending0(date.substr(0, 3)) + '0s')
+      }
+    }
+    else if (date.match(/^\d{4}s$/)) {
+      let cent = parseInt(date.substr(0, 2)) + 1
+      possibilities = possibilities.concat(parts('C' + cent , options))
+
+      let p = range(Math.floor((date.substr(2, 1) + '9') / 100 * 3) + 1, 2)
+        .map(i => earlyMidLate[i])
+      if (!options.strict && date.substr(2, 1) !== '9') {
+        p.push(earlyMidLate[Math.floor((date.substr(2, 1) + '9') / 100 * 3)])
+      }
+      if (p.length) {
+        possibilities.push('(' + arrayUnique(p).join(' |') + ' )C' + cent)
+      }
+
+      if (date[2] < 9) {
+        possibilities.push('(|early |mid |late )' + date.substr(0, 2) + regexpRange(parseInt(date[2]) + 1, 9) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
+      }
+      if (date[0] === '9') {
+        possibilities.push('(|early |mid |late )' + date.substr(1, 1) + regexpRange(parseInt(date[2]) + 1, 9) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
+      }
+
+      if (!options.strict) {
+        possibilities.push('(|early |mid |late )C' + optPending0(('' + cent).padStart('0', 2)))
+      }
+    }
+    else if (date.match(/^C\d+$/)) {
+      let cent = date.substr(1).padStart(2, '0')
+      let year = ('' + ((cent - 1) * 100)).padStart(4, '0')
+
+      for (let i = 0; i < 2; i++) {
+        if (cent[i] < (i === 0 ? maxCent[i] : 9)) {
+          possibilities.push('(|early |mid |late )C' + optPending0(cent.substr(0, i)) + regexpRange(parseInt(cent[i]) + 1, i == 0 ? maxCent[i] : 9) + '[0-9]'.repeat(1 - i))
+        }
+
+        if (year[i] < (i === 0 ? maxYear[i] : 9)) {
+          possibilities.push('(|early |mid |late )' + optPending0(year.substr(0, i)) + regexpRange(parseInt(year.substr(i, 1)) + 1, i == 0 ? maxYear[i] : 9) + '[0-9]'.repeat(2 - i) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
+        }
+      }
+    }
+  }
 
   return possibilities
 }
@@ -277,7 +392,6 @@ function osmDateQuery (date, options={}) {
   if (!('strict' in options)) {
     options.strict = false
   }
-
 
   if (!options.op || options.op === '=') {
     if (options.strict) {
@@ -300,6 +414,14 @@ function osmDateQuery (date, options={}) {
       return '^(|.*\\.\\.)(' + result.join('|') + ')$'
     } else {
       return '^(' + result.join('|') + ')(|\\.\\..*)$'
+    }
+  } else if (options.op === '>' || options.op === '>=') {
+    let result = parts(date, options)
+
+    if (options.strict) {
+      return '^(' + result.join('|') + ')(|\\.\\..*)$'
+    } else {
+      return '^(|.*\\.\\.)(' + result.join('|') + ')$'
     }
   }
 }
--- test/test.js
+++ test/test.js
@@ -402,3 +402,263 @@ describe('<=, strict=false', function () {
     )
   })
 })
+
+describe('>, strict=true', function () {
+  it('all dates after C20', function () {
+    let regexp = osmDateQuery('C20', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C21', 'C22', '2010s', '2000s', '2020s', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates after 2019-09-22', function () {
+    let regexp = osmDateQuery('2019-09-22', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019-12-24', '2020-12-24', 'C22', '2020s' ],
+      matches
+    )
+  })
+
+  it('all dates after 2019-09-21', function () {
+    let regexp = osmDateQuery('2019-09-21', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019-09-22', '2019-12-24', '2020-12-24', 'C22', '2020s', 'late 2019-09' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 2019', function () {
+    let regexp = osmDateQuery('2019', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2020-12-24', 'C22', '2020s' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 2017', function () {
+    let regexp = osmDateQuery('2017', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', 'C22', '2020s', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 2099', function () {
+    let regexp = osmDateQuery('2099', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ 'C22' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 1506', function () {
+    let regexp = osmDateQuery('1506', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates after the 1510s', function () {
+    let regexp = osmDateQuery('1510s', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 400', function () {
+    let regexp = osmDateQuery('0400', { op: '>', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C15', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+})
+
+describe('>, strict=false', function () {
+  it('all dates after 2019-09-22', function () {
+    let regexp = osmDateQuery('2019-09-22', { op: '>', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-09', '2019-07..2019-10', '2019-12-24', '2020-12-24', 'C21', 'C22', '2010s', '2020s', '1988..2020', 'late 2019', 'late 2019-09', 'late 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 2019', function () {
+    let regexp = osmDateQuery('2019', { op: '>', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2020-12-24', 'C21', 'C22', '2020s', '1988..2020', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 1506', function () {
+    let regexp = osmDateQuery('1506', { op: '>', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+
+  it('all dates after the 1510s', function () {
+    let regexp = osmDateQuery('1510s', { op: '>', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+
+  it('all dates after the year 400', function () {
+    let regexp = osmDateQuery('0400', { op: '>', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C15', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+})
+
+describe('>=, strict=true', function () {
+  it('all dates >= 2019-09-21', function () {
+    let regexp = osmDateQuery('2019-09-21', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019-09-21', '2019-09-22', '2019-12-24', '2020-12-24', 'C22', '2020s', 'late 2019-09' ],
+      matches
+    )
+  })
+
+  it('all dates >= 2019-09', function () {
+    let regexp = osmDateQuery('2019-09', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019-09', '2019-09-01..2019-09-21', '2019-09-21', '2019-09-22', '2019-12-24', '2020-12-24', 'C22', '2020s', 'late 2019-09' ],
+      matches
+    )
+  })
+
+  it('all dates >= 2019', function () {
+    let regexp = osmDateQuery('2019', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2020-12-24', 'C22', '2020s', 'early 2019', 'mid 2019', 'late 2019', 'mid 2019', 'late 2019-09' ],
+      matches
+    )
+  })
+
+  it('all dates >= 1507', function () {
+    let regexp = osmDateQuery('1507', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates >= 1500s', function () {
+    let regexp = osmDateQuery('1500s', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates >= 350', function () {
+    let regexp = osmDateQuery('0350', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C15', 'C20', 'C21', 'C22', '1507-01-01', '0350-01-01', '350-01-01', '350', '0350', '1500s', '0360s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'late 350', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16', 'late C4' ],
+      matches
+    )
+  })
+
+  it('all dates >= C21', function () {
+    let regexp = osmDateQuery('C21', { op: '>=', strict: true })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C21', 'C22', '2010s', '2000s', '2020s', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+})
+
+describe('>=, strict=false', function () {
+  it('all dates >= 2019-09-21', function () {
+    let regexp = osmDateQuery('2019-09-21', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-12-24', '2020-12-24', 'C21', 'C22', '2010s', '2020s', '1988..2020', 'late 2019', 'late 2019-09', 'late 2010s', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates >= 2019-09', function () {
+    let regexp = osmDateQuery('2019-09', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-12-24', '2020-12-24', 'C21', 'C22', '2010s', '2020s', '1988..2020', 'late 2019', 'late 2019-09', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates >= 2019', function () {
+    let regexp = osmDateQuery('2019', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2020-12-24', 'C21', 'C22', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'mid 2019', 'late 2019-09', 'early C21' ],
+      matches
+    )
+  })
+
+  it('all dates >= 1507', function () {
+    let regexp = osmDateQuery('1507', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+
+  it('all dates >= 1500s', function () {
+    let regexp = osmDateQuery('1500s', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C20', 'C21', 'C22', '1507-01-01', '1500s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16' ],
+      matches
+    )
+  })
+
+  it('all dates >= 350', function () {
+    let regexp = osmDateQuery('0350', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C15', 'C20', 'C21', 'C22', '1507-01-01', '0350-01-01', '350-01-01', '350', '0350', '1500s', '0360s', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'late 1505', 'late 350', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21', 'early C16', 'late C4' ],
+      matches
+    )
+  })
+
+  it('all dates >= C21', function () {
+    let regexp = osmDateQuery('C21', { op: '>=', strict: false })
+    let matches = testAll(regexp)
+    assert.deepEqual(
+      [ '2019', '2019-07', '2019-08', '2019-09', '2019-09-01..2019-09-21', '2019-07..2019-10', '2019-09-21', '2019-09-22', '2019-01-01', '2019-12-24', '2018-12-24', '2020-12-24', '2008-12-24', '2008-01-01..2010-01-01', '2008-08', 'C21', 'C22', '2010s', '2000s', '2020s', '1988..2020', 'early 2019', 'mid 2019', 'late 2019', 'late 2018', 'mid 2019', 'late 2019-09', 'late 2010s', 'early 2000s', 'early 2010s', 'early C21' ],
+      matches
+    )
+  })
+})
