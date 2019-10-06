57c4e28fa47aa21a2cbee721fa6ca399b22e886c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-10-06 22:22:47 +0200

    Document each added regular expression (incl. some cleanup) UNFIN

--- src/query.js
+++ src/query.js
@@ -9,25 +9,22 @@ const range = require('./range')
 const optionalLeadingZero = require('./optionalLeadingZero')
 
 function buildEqDate (date, options, possibilities) {
+  // same day
   possibilities.push(optionalLeadingZero(date))
 
   if (!options.strict) {
     let p
-    // p - part of the month
+
+    // same part of current month, resp. month only
     p = earlyMidLate[Math.floor(date.substr(8, 2) / 31 * 3)]
     possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 7)))
-    // p - part of the year
+    // same part of this year, resp. year only
     p = earlyMidLate[Math.floor((date.substr(5, 2) - 1) / 12 * 3)]
     possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 4)))
-
-    p = earlyMidLate[Math.floor((date.substr(5, 2) - 1) / 12 * 3)]
-    possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 4)))
-
-    possibilities.push(optionalLeadingZero(date.substr(0, 3) + '0s'))
-
+    // same part of this decade, resp. decade only
     p = earlyMidLate[Math.floor((date.substr(3, 1) / 10 + (date.substr(5, 2) - 1) / 120) * 3)]
     possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 3)) + '0s')
-
+    // same part of this century, resp. century only
     const cent = parseInt(date.substr(0, 2)) + 1
     p = earlyMidLate[Math.floor((date.substr(2, 2) / 100 + (date.substr(5, 2) - 1) / 1200) * 3)]
     possibilities.push('(|' + p + ' )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
@@ -35,17 +32,19 @@ function buildEqDate (date, options, possibilities) {
 }
 
 function buildEqMonth (date, options, possibilities) {
+  // same month
   possibilities.push('(|early |mid |late )' + optionalLeadingZero(date + '(-[0-9]{2})?'))
 
   if (!options.strict) {
     let p
 
+    // same part of this year, resp. year only
     p = earlyMidLate[Math.floor((date.substr(5, 2) - 1) / 12 * 3)]
     possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 4)))
-
+    // same part of this decade, resp. decade only
     p = earlyMidLate[Math.floor((date.substr(3, 1) / 10 + (date.substr(5, 2) - 1) / 120) * 3)]
     possibilities.push('(|' + p + ' )' + optionalLeadingZero(date.substr(0, 3)) + '0s')
-
+    // same part of this century, resp. century only
     const cent = parseInt(date.substr(0, 2)) + 1
     p = earlyMidLate[Math.floor((date.substr(2, 2) / 100 + (date.substr(5, 2) - 1) / 1200) * 3)]
     possibilities.push('(|' + p + ' )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
@@ -53,42 +52,45 @@ function buildEqMonth (date, options, possibilities) {
 }
 
 function buildEqYear (date, options, possibilities) {
+  // same year
   possibilities.push('(|early |mid |late )' + optionalLeadingZero(date + '(-[0-9]{2}(-[0-9]{2})?)?'))
 
   if (!options.strict) {
     let p
 
+    // same part(s) of this decade, resp. decade only
     p = arrayUnique([
       earlyMidLate[Math.floor((date.substr(3, 1)) / 10 * 3)],
       earlyMidLate[Math.floor((date.substr(3, 1) + '.99') / 10 * 3)]
     ])
     possibilities.push('(|' + p.join(' |') + ' )' + optionalLeadingZero(date.substr(0, 3)) + '0s')
-
+    // same part(s) of this century, resp. century only
     p = arrayUnique([
       earlyMidLate[Math.floor((date.substr(2, 2)) / 100 * 3)],
       earlyMidLate[Math.floor((date.substr(2, 2) + '.99') / 100 * 3)]
     ])
-
     const cent = parseInt(date.substr(0, 2)) + 1
     possibilities.push('(|' + p.join(' |') + ' )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
   }
 }
 
 function buildEqDecade (date, options, possibilities) {
+  // same decade
   possibilities.push('(|early |mid |late )' + optionalLeadingZero(date.substr(0, 3)) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
 
   if (!options.strict) {
+    // same part(s) of this century, resp. century only
     const p = arrayUnique([
       earlyMidLate[Math.floor((date.substr(2, 1) + '0') / 100 * 3)],
       earlyMidLate[Math.floor((date.substr(2, 1) + '9') / 100 * 3)]
     ])
-
     const cent = parseInt(date.substr(0, 2)) + 1
     possibilities.push('(|' + p.join(' |') + ' )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
   }
 }
 
 function buildEqCentury (date, options, possibilities) {
+  // same century - either as 'Cxx' or 'YYYY-...'
   const cent = date.substr(1).padStart('0', 2)
   const year = ('' + ((cent - 1) * 100)).padStart(4, '0')
   possibilities.push('(|early |mid |late )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
@@ -96,6 +98,7 @@ function buildEqCentury (date, options, possibilities) {
 }
 
 function buildLtDate (date, options, possibilities) {
+  // all earlier part(s) of the current month
   const p = range(0, Math.floor((date.substr(8, 2) - 1) / 31 * 3) - 1)
     .map(i => earlyMidLate[i])
   if (!options.strict && date.substr(8, 2) !== '01') {
@@ -105,16 +108,19 @@ function buildLtDate (date, options, possibilities) {
     possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 7))
   }
 
+  // all dates in the current month before current day
   if (date.substr(8, 2) !== '01') {
     possibilities.push(optionalLeadingZero(date.substr(0, 8) + regexpRangeDouble(1, date.substr(8, 2) - 1)))
   }
 
+  // month only
   if (!options.strict) {
     possibilities.push(optionalLeadingZero(date.substr(0, 7)))
   }
 }
 
 function buildLtMonth (date, options, possibilities) {
+  // all earlier part(s) of the current year
   const p = range(0, Math.floor((date.substr(5, 2) - 1) / 12 * 3) - 1)
     .map(i => earlyMidLate[i])
   if (!options.strict && date.substr(5, 2) !== '01') {
@@ -124,16 +130,19 @@ function buildLtMonth (date, options, possibilities) {
     possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 4))
   }
 
+  // all months in the current year before current month
   if (date.substr(5, 2) !== '01') {
     possibilities.push(optionalLeadingZero(date.substr(0, 5)) + regexpRangeDouble(1, date.substr(5, 2) - 1) + '(-[0-9]{2})?')
   }
 
+  // year only
   if (!options.strict) {
     possibilities.push(optionalLeadingZero(date.substr(0, 4)))
   }
 }
 
 function buildLtYear (date, options, possibilities) {
+  // all earlier part(s) of the current decade
   const p = range(0, Math.floor((date.substr(3, 1)) / 10 * 3) - 1)
     .map(i => earlyMidLate[i])
   if (!options.strict && date.substr(3, 1) !== '0') {
@@ -143,16 +152,19 @@ function buildLtYear (date, options, possibilities) {
     possibilities.push('(' + arrayUnique(p).join(' |') + ' )' + date.substr(0, 3) + '0s')
   }
 
+  // all years in the current decade before current year
   if (date[3] !== '0') {
     possibilities.push('(|early |mid |late )' + optionalLeadingZero(date.substr(0, 3)) + regexpRange(0, date[3] - 1) + '(-[0-9]{2}(-[0-9]{2})?)?')
   }
 
+  // decade only
   if (!options.strict) {
     possibilities.push('(|early |mid |late )' + optionalLeadingZero(date.substr(0, 3)) + '0s')
   }
 }
 
 function buildLtDecade (date, options, possibilities) {
+  // all earlier part(s) of the current century
   const cent = parseInt(date.substr(0, 2)) + 1
   const p = range(0, Math.floor((date.substr(2, 1) + '0') / 100 * 3) - 1)
     .map(i => earlyMidLate[i])
@@ -163,32 +175,44 @@ function buildLtDecade (date, options, possibilities) {
     possibilities.push('(' + arrayUnique(p).join(' |') + ' )[Cc]' + cent)
   }
 
+  // all decades in the current century before current decade
   if (date[2] > 0) {
-    possibilities.push('(|early |mid |late )' + date.substr(0, 2) + regexpRange(0, date[2] - 1) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
-  }
-  if (date[0] === '0' && date[2] > 0) {
-    possibilities.push('(|early |mid |late )' + date.substr(1, 1) + regexpRange(0, date[2] - 1) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
+    possibilities.push('(|early |mid |late )' + optionalLeadingZero(date.substr(0, 2)) + regexpRange(0, date[2] - 1) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
   }
 
+  // century only
   if (!options.strict) {
     possibilities.push('(|early |mid |late )[Cc]' + optionalLeadingZero(('' + cent).padStart('0', 2)))
   }
 }
 
 function buildLtCentury (date, options, possibilities) {
+  // all dates BCE
   possibilities.push('.* [Bb][Cc][Ee]?')
 
   const cent = date.substr(1).padStart(2, '0')
   const year = ('' + ((cent - 1) * 100)).padStart(4, '0')
 
-  for (let i = 0; i < 2; i++) {
-    if (cent[i] > 0) {
-      possibilities.push('(|early |mid |late )[Cc]' + optionalLeadingZero(cent.substr(0, i) + '0') + '[0-9]'.repeat(1 - i))
-    }
-    if (cent[i] > 1) {
-      possibilities.push('(|early |mid |late )[Cc]' + optionalLeadingZero(cent.substr(0, i)) + regexpRange(1, cent[i] - 1) + '[0-9]'.repeat(1 - i))
-    }
+  // all (earlier?) centuries in first millenium
+  if (cent[0] === '0') {
+    possibilities.push('(|early |mid |late )[Cc]0?' + regexpRange(1, cent[1] - 1))
+  } else {
+    possibilities.push('(|early |mid |late )[Cc]0?[1-9]')
+  }
 
+  // all millenia between first and current millenium
+  if (cent[0] > 1) {
+    possibilities.push('(|early |mid |late )[Cc]' + regexpRange(1, cent[0] - 1) + '[0-9]')
+  }
+
+  // all earlier centuries in current millenium
+  if (cent[0] > 0 && cent[1] > 0) {
+    possibilities.push('(|early |mid |late )[Cc]' + optionalLeadingZero(cent[0]) + optionalLeadingZero(regexpRange(0, cent[1] - 1)))
+  }
+
+  // i=0: centuries C0..C9
+  // i=1: centuries C10..
+  for (let i = 0; i < 2; i++) {
     if (year[i] > 0) {
       possibilities.push('(|early |mid |late )' + optionalLeadingZero(year.substr(0, i) + '0') + '[0-9]'.repeat(2 - i) + '(0s|[0-9](-[0-9]{2}(-[0-9]{2})?)?)')
     }
